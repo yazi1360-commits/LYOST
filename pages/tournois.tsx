e2e3a8543c35c827fa8c6c2696c919eafda14e57
yazi1360-commits <yazi1360@gmail.com>
2026-02-23 17:05:57 +0100

    Create tournaments page

---
+++ pages/tournois.tsx
@@ -0,0 +1,14 @@
+// This is the tournaments page component
+
+import React from 'react';
+
+const Tournaments = () => {
+    return (
+        <div>
+            <h1>Tournaments</h1>
+            <p>Welcome to the tournaments page!</p>
+        </div>
+    );
+};
+
+export default Tournaments;
