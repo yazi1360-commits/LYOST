3193e07a4ff28b5c1e8856ed91c511055267c991
yazi1360-commits <yazi1360@gmail.com>
2026-02-24 12:43:32 +0100

    Create models/User.ts

--- models/User.ts
+++ models/User.ts
@@ -1,31 +1,12 @@
 import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    points: {
-      type: Number,
-      default: 0,
-    },
-    badges: {
-      type: [String],
-      default: [],
-    },
+    username: { type: String, required: true, unique: true, },
+    email: { type: String, required: true, unique: true, },
+    password: { type: String, required: true, },
+    points: { type: Number, default: 0, },
+    badges: { type: [String], default: [], },
   },
   { timestamps: true }
 );
-
-export default mongoose.models.User || mongoose.model('User', userSchema);
+export default mongoose.models.User || mongoose.model('User', UserSchema);
